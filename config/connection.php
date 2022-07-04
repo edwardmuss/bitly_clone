d3981af5f85a7e1f18a81904cb8ee69b9dd93d81
edwardmuss <edwardmuss5@gmail.com>
2022-07-04 16:25:08 +0300

    create connection

---
+++ config/connection.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @param servername (Host)
+ * @param username (db user)
+ * @param dbname (database name)
+ * @param password (database Password)
+ * @param base_url (root url)
+ */
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'bitly_clone';
+$base_url='http://localhost/bitly_clone/'; 
